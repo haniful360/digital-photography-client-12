2d936c2b492d79853c1e89c9495e97602916ae5d
haniful360 <haniful360@gmail.com>
2023-06-09 03:16:45 +0600

    isAdmin isInstructor students conditionally added

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query";
+import useAuth from "./useAuth";
+
+
+const useAdmin = () => {
+    const {user} = useAuth();
+    const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
+        queryKey: ['isAdmin', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/users/admin/${user?.email}`);
+            // console.log('is admin response', res)
+            return res.json();
+        }
+    })
+    return [isAdmin, isAdminLoading]
+}
+export default useAdmin;
--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { FaUsers } from 'react-icons/fa';
 import { AuthContext } from '../providers/AuthProviders';
+import useAdmin from '../hooks/useAdmin';
 
 const Dashboard = () => {
-    // const {user} = useState(AuthContext);
-    
+    // const {user} = useContext(AuthContext);
+    // console.log(user.role);
+    const isAdmin = false;
+    const isInstructor = false;
+    // console.log(isAdmin);
+
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -18,26 +23,25 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 h-full w-80 text-base-content bg-[#134ba5cb]">
 
-                        
-                        {isAdmin ? <>
-                            <li><NavLink to='/'><h2 className='text-3xl'>Photography</h2></NavLink></li>
 
 
-                            
-                            <li><NavLink to='/dashboard/manageUsers'>Manage users</NavLink></li>
-                            <li><NavLink to='/dashboard/manageClasses'>Manage Classes</NavLink></li>
-                        </> : <>
+                    {isAdmin ? <><li><NavLink to='/'><h2 className='text-3xl'>Photography</h2></NavLink></li>
 
+                        <li><NavLink to='/dashboard/manageUsers'>Manage users</NavLink></li>
+                        <li><NavLink to='/dashboard/manageClasses'>Manage Classes</NavLink></li></> : isInstructor ? <>
                             <li><NavLink to='/'><h2 className='text-3xl'> Photography</h2></NavLink></li>
 
-                            <li><NavLink to='/dashboard/enrolledClasses'>My Enrolled Classes</NavLink></li>
-                            <li><NavLink to='/dashboard/selectedClasses'>My selected Classes</NavLink></li>
-                            
-                        </>
-                        }
-                        
-                    </ul>
+                            <li><NavLink to='/dashboard/addClass'>Add a Class</NavLink></li>
+                            <li><NavLink to='/dashboard/myClass'>My  Class</NavLink></li>
+                        </> : <><li><NavLink to='/'><h2 className='text-3xl'> Photography</h2></NavLink></li>
 
+                        <li><NavLink to='/dashboard/enrolledClasses'>My Enrolled Classes</NavLink></li>
+                        <li><NavLink to='/dashboard/selectedClasses'>My selected Classes</NavLink></li></>}
+
+
+
+
+                </ul>
             </div>
         </div>
     );
