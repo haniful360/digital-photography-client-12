6587e997d7ea557ae4da56f41cae1f707714b7f3
haniful360 <haniful360@gmail.com>
2023-06-14 22:24:50 +0600

    readme.md file added

---
+++ README.md
@@ -0,0 +1,15 @@
+# toy  website: Digital Photography
+# My website link:https://summer-camp-scl-12.web.app/
+# description:
+
+*Users need to login via Github or Google (also can create a  new Account). 
+
+This is a photography tutorial website.
+There will be admin and moderator here.
+A moderator can add many classes and that class will be pending, admin can accept and reject if he wants.
+Admin can control everything.
+Any student can purchase the classes accepted by admin moderators
+
+*Technology used : React.js, Node.js, Express.js, Tailwind, CSS, Daisy UI, Dotenv, Firebase Auth, MongoDB, React Router, JWT etc react query axios.
+
+
--- src/Pages/Home/PopularInstructor/PopularInstructor.jsx
+++ src/Pages/Home/PopularInstructor/PopularInstructor.jsx
@@ -5,14 +5,17 @@ const PopularInstructor = () => {
     useEffect(() => {
         fetch('http://localhost:5000/users')
             .then(res => res.json())
-            .then(data => setPopularInstructor(data))
+            .then(data => {
+                const instructor = data.filter(ins => ins.role === "instructor")
+                setPopularInstructor(instructor);
+            })
     }, [])
     return (
         <div className='popular-instructor my-12'>
             <h1 className='text-center'>popular Instructor:{popularInstructor.length}</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
-                    popularInstructor.slice(0,6).map(popularIns => <PopularInsDetails key={popularIns._id}
+                    popularInstructor.slice(0, 6).map(popularIns => <PopularInsDetails key={popularIns._id}
                         popularIns={popularIns}
                     ></PopularInsDetails>)
                 }
